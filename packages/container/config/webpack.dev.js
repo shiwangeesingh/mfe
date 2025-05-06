9314275719b7b996cc6a500020c3410b6773a3da
shiwangee.singh <shiwangee.singh@hcl.com>
2025-05-06 15:45:46 +0530

    deploy conatiner

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const {merge} = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
@@ -18,20 +17,11 @@ const devConfig = {
             remotes:{
                 marketing: "marketing@http://localhost:8081/remoteEntry.js"
             },
-            // filename: "remoteEntry.js",
             exposes: {
                 "./Container": "./src/bootstrap"
             },
             shared:packageJson.dependencies
-            // {
-            //     faker:{
-            //         singleton:true
-            //     } 
-            // }
-        }),
-        new HtmlWebpackPlugin({
-            template: "./public/index.html",
-          })
+        })
     ]
 }
 
